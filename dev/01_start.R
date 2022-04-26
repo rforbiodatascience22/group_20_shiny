ed229f521a19b9b06fbc0b2a1f31a6519c128c26
MOLS <orie.simpson@gmail.com>
2022-04-26 13:22:09 +0200

    Changing the values in the fill description part 13.22

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,12 +20,12 @@
 ##
 golem::fill_desc(
   pkg_name = "ShinyMolBio", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional)
+  pkg_title = "The Shiny MolBio Website App", # The Title of the package containing the App
+  pkg_description = "The Central Dogma of Molecular Biology", # The Description of the package containing the App
+  author_first_name = "Oriade", # Your First Name
+  author_last_name = "Simpson", # Your Last Name
+  author_email = "s172084@dtu.dk", # Your Email
+  repo_url = "https://github.com/rforbiodatascience22/group_20_shiny"
 )
 
 ## Set {golem} options ----
@@ -33,11 +33,11 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_mit_license("Miss Oriade Latifah Simpson") # You can set another license here
 usethis::use_readme_rmd(open = FALSE)
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
-usethis::use_code_of_conduct(contact = "Golem User")
+usethis::use_code_of_conduct(contact = "Miss Oriade Latifah Simpson")
 usethis::use_lifecycle_badge("Experimental")
 usethis::use_news_md(open = FALSE)
 
