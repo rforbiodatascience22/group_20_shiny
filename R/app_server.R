c5be4706f5aeab03ea8cb06b77816f466745652d
MOLS <orie.simpson@gmail.com>
2022-04-27 16:06:43 +0200

    The Server Function Changed based on Prototype 16.06

--- R/app_server.R
+++ R/app_server.R
@@ -6,4 +6,20 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
+  # Make the number 10
+  numb <- reactive(input$a * 2)
+
+  # use the module to make the DNA to protein.
+  dna <- reactive(make_dna(numb()))
+  rna <- reactive(make_rna(dna()))
+  codon <- reactive(make_codon(rna()))
+  protein <- reactive(make_polypeptide(codon()))
+  chart <- reactive(find_aa_distribution(protein()))
+
+  # View the outcomes
+  output$oDNA <- renderText(dna())
+  output$oRNA <- renderText(rna())
+  output$oCodon <- renderText(codon())
+  output$oProt <- renderText(protein())
+  output$obar <- renderPlot(chart(), res = 96)
 }
