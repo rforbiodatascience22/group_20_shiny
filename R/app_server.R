b9562f3e886dc82e7fee45d1ed20fe19d1d9c5a1
MOLS <orie.simpson@gmail.com>
2022-04-27 16:08:46 +0200

    Add the MolBio Package to the imports to enable functions to work 16.08

--- R/app_server.R
+++ R/app_server.R
@@ -3,6 +3,7 @@
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @import MolBio
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
