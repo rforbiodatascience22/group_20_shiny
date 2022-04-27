729835faaf7acce10b68feecf6df30cdd04a5a73
MOLS <orie.simpson@gmail.com>
2022-04-27 16:05:35 +0200

    The App UI changed. 16.05

--- R/app_ui.R
+++ R/app_ui.R
@@ -14,9 +14,25 @@ app_ui <- function(request) {
       p("Choose a number to represent the quantity of nucleotides in a simulated DNA sequence", class = "my-class1"),
 
       # create an slider asking for a number
-      sliderInput("a", "Move the slider to enter a number: ", value = 50, min = 0, max = 100)
+      sliderInput("a", "Move the slider to enter a number: ", value = 50, min = 0, max = 100),
 
       # ----------------------------------------------
+      textOutput("a"),
+      textOutput("b"),
+      textOutput("c"),
+
+      p("The DNA sequence: ", class = "my-class2"),
+      verbatimTextOutput("oDNA"),
+      p("The RNA sequence: ", class = "my-class3"),
+      verbatimTextOutput("oRNA"),
+      p("The Codon sequence: ", class = "my-class4"),
+      verbatimTextOutput("oCodon"),
+      p("The Polypeptide sequence: ", class = "my-class5"),
+      verbatimTextOutput("oProt"),
+      p("The Distribution of Amino Acids: ", class = "my-class6"),
+      plotOutput("obar"),
+      p("", class = "my-class6"),
+      p("The Shiny MolBio App outlines the Central Dogma of Molecular Biology", class = "my-class8"),
     )
   )
 }
